71f6f80e64861651689fb9e49c58628112bcea3e
Mykle jones <mykle.j91@gmail.com>
2019-08-17 15:35:27 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -20,7 +20,8 @@ else
  end
 end
 
-def find_the_cheese# code an argument here
-  # the array below is here to help
+def find_the_cheese(strings)
   cheese_types = ["cheddar", "gouda", "camembert"]
+  
+  
 end
