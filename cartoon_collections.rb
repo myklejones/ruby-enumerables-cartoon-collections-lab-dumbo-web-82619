2d1792aed1ff944f2be1c474ed955a7fccaa36b5
Mykle jones <mykle.j91@gmail.com>
2019-08-17 15:04:56 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -12,8 +12,10 @@ def summon_captain_planet(planeteer_calls)
  
 end
 
-def long_planeteer_calls# code an argument here
-  # Your code here
+def long_planeteer_calls(calls)
+  
+  
+  
 end
 
 def find_the_cheese# code an argument here
