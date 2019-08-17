45067fa31aa9f465759ba149e11ba6c598c2a7b1
Mykle jones <mykle.j91@gmail.com>
2019-08-17 15:16:21 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -13,7 +13,7 @@ def summon_captain_planet(planeteer_calls)
 end
 
 def long_planeteer_calls(calls)
-  if calls.all?{|string|string >= 4}
+  if calls.all?{|word|word  >= 4}
      true 
   else 
      false 
